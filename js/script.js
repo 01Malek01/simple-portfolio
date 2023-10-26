6a7a01ab146c039aad4e25a7f3402f53958e7022
Malek <stonemokkaks@gmail.com>
2023-10-26 18:12:42 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,8 @@
+
+//get btn
+let topBtn = document.getElementsByClassName("scrll-tp");
+//define the function
+function goTop(){
+    document.scrollTop=0;
+    document.documentElement.scrollTop=0;
+}
